18bfaf44bcee0d4d3aae4b14918ee5f48333b97d
Justin Meyers <jme196@uclive.ac.nz>
2022-11-05 16:59:28 +1300

    Cleaned out checking win code. Each check is now seperate function instead of one long one.

--- connect_four.py
+++ connect_four.py
@@ -7,6 +7,77 @@ def setup_board():
     board = [["E" for i in range(BOARD_X)] for j in range(BOARD_Y)]
     return board
 
+def down_check_win(x, y, board, turn):
+    has_won = True
+    for temp_y in range(y, y + 4):
+        if (temp_y >= len(board)) or (board[temp_y][x] != turn[0]):
+            has_won = False
+            break
+    if (has_won):
+        return True
+
+def left_check_win(x, y, board, turn):
+    has_won = True
+    for temp_x in range(x, x - 4, -1):
+        if (temp_x < 0) or (board[y][temp_x] != turn[0]):
+            has_won = False
+            break
+    if (has_won):
+        return True
+
+def right_check_win(x, y, board, turn):
+    has_won = True
+    for temp_x in range(x, x + 4):
+        if (temp_x >= len(board[0])) or (board[y][temp_x] != turn[0]):
+            has_won = False
+            break
+    if (has_won):
+        return True
+
+def topleft_check_win(x, y, board, turn):
+    has_won = True
+    temp_y = y
+    for temp_x in range(x, x - 4, -1):
+        if (temp_x < 0 or temp_y < 0) or (board[temp_y][temp_x] != turn[0]):
+            has_won = False
+            break
+        temp_y -= 1
+    if (has_won):
+        return True
+
+def topright_check_win(x, y, board, turn):
+    has_won = True
+    temp_y = y
+    for temp_x in range(x, x + 4):
+        if (temp_x >= len(board[0]) or temp_y < 0) or (board[temp_y][temp_x] != turn[0]):
+            has_won = False
+            break
+        temp_y -= 1
+    if (has_won):
+        return True
+
+def bottomleft_check_win(x, y, board, turn):
+    has_won = True
+    temp_y = y
+    for temp_x in range(x, x - 4, -1):
+        if (temp_x < 0 or temp_y >= len(board)) or (board[temp_y][temp_x] != turn[0]):
+            has_won = False
+            break
+        temp_y += 1
+    if (has_won):
+        return True
+
+def bottomright_check_win(x, y, board, turn):
+    has_won = True
+    temp_y = y
+    for temp_x in range(x, x + 4):
+        if (temp_x >= len(board[0]) or temp_y >= len(board)) or (board[temp_y][temp_x] != turn[0]):
+            has_won = False
+            break
+        temp_y += 1
+    if (has_won):
+        return True
+
 class Game:
     def __init__(self, board):
         self.turn = "Red"
@@ -25,62 +96,19 @@ class Game:
         return self.board
 
     def check_win(self, x, y):
-        has_won = True
-        for temp_y in range(y, y + 4): # down
-            if (temp_y >= len(self.board)) or (self.board[temp_y][x] != self.get_turn()[0]):
-                has_won = False
-                break
-        if (has_won):
+        if down_check_win(x, y, self.board, self.get_turn()):
             return True
-        has_won = True
-        for temp_x in range(x, x - 4, -1): # left
-            if (temp_x < 0) or (self.board[y][temp_x] != self.get_turn()[0]):
-                has_won = False
-                break
-        if (has_won):
+        if left_check_win(x, y, self.board, self.get_turn()):
             return True
-        has_won = True
-        for temp_x in range(x, x + 4): # right
-            if (temp_x >= len(self.board[0])) or (self.board[y][temp_x] != self.get_turn()[0]):
-                has_won = False
-                break
-        if (has_won):
+        if right_check_win(x, y, self.board, self.get_turn()):
             return True
-        has_won = True
-        temp_y = y
-        for temp_x in range(x, x - 4, -1): # top left
-            if (temp_x < 0 or temp_y < 0) or (self.board[temp_y][temp_x] != self.get_turn()[0]):
-                has_won = False
-                break
-            temp_y -= 1
-        if (has_won):
+        if topleft_check_win(x, y, self.board, self.get_turn()):
             return True
-        has_won = True
-        temp_y = y
-        for temp_x in range(x, x + 4): # top right
-            if (temp_x >= len(self.board[0]) or temp_y < 0) or (self.board[temp_y][temp_x] != self.get_turn()[0]):
-                has_won = False
-                break
-            temp_y -= 1
-        if (has_won):
+        if topright_check_win(x, y, self.board, self.get_turn()):
             return True
-        has_won = True
-        temp_y = y
-        for temp_x in range(x, x + 4): # bottom right
-            if (temp_x >= len(self.board[0]) or temp_y >= len(self.board)) or (self.board[temp_y][temp_x] != self.get_turn()[0]):
-                has_won = False
-                break
-            temp_y += 1
-        if (has_won):
+        if bottomleft_check_win(x, y, self.board, self.get_turn()):
             return True
-        has_won = True
-        temp_y = y
-        for temp_x in range(x, x - 4, -1): # bottom left
-            if (temp_x < 0 or temp_y >= len(self.board)) or (self.board[temp_y][temp_x] != self.get_turn()[0]):
-                has_won = False
-                break
-            temp_y += 1
-        if (has_won):
+        if bottomright_check_win(x, y, self.board, self.get_turn()):
             return True
         return False
 
@@ -108,7 +136,7 @@ class Game:
 def main():
     board = setup_board()
     game = Game(board)
-    game_view = gv.GameView(game)
+    gv.GameView(game)
 
 if __name__ == "__main__":
     main()
